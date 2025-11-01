bb6bf02c59fd5553870cb642b82866c174a707f2
iotdepartment <iotdepartmenttg@gmail.com>
2025-11-01 11:05:01 -0500

    Segundo commit cadena

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -31,7 +31,7 @@ public partial class AppDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=10.195.10.166,1433;Database=ScanSystemDb_Copy;Trusted_Connection=True;TrustServerCertificate=True;User ID=Produccion;Password=produ2025!tg.");
+        => optionsBuilder.UseSqlServer("Server=10.195.10.166,1433;Database=ScanSystemDb_Copy;Trusted_Connection=True;TrustServerCertificate=True;User ID=Produccionn;Password=2025sqlserver");
 
     //"Server=RMX-D4LZZV2;Database=ScanSystemDb;Trusted_Connection=True;TrustServerCertificate=True;User ID=eramirez3;Password=2022.Tgram2."
 
