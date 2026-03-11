3daaf8e214f6b7289f9ca238f2ecf17f208f7a93
iotdepartment <iotdepartmenttg@gmail.com>
2026-03-10 21:36:28 -0500

    Prueba zona horario helper

--- Helpers/TimeZoneHelper.cs
+++ Helpers/TimeZoneHelper.cs
@@ -4,14 +4,16 @@ namespace ProducScan.Helpers
 {
     public static class TimeZoneHelper
     {
-        // Obtiene la hora local del sistema con offset
-        public static DateTimeOffset GetLocalTime()
+        // Obtiene la hora correcta de Matamoros aplicando reglas personalizadas
+        public static DateTime GetMatamorosTime()
         {
-            return DateTimeOffset.Now;
+            DateTime utcNow = DateTime.UtcNow;
+            TimeSpan offset = GetMatamorosOffset(utcNow);
+            return utcNow.Add(offset);
         }
 
         // Determina el turno según la hora
-        public static string GetTurno(DateTimeOffset fechaLocal)
+        public static string GetTurno(DateTime fechaLocal)
         {
             var horaActual = fechaLocal.TimeOfDay;
 
@@ -21,6 +23,37 @@ namespace ProducScan.Helpers
                 return "Turno 2";
             else
                 return "Turno 3";
+        }
+
+        // Reglas de horario de verano en Matamoros:
+        // - Inicia: segundo domingo de marzo (UTC-5)
+        // - Termina: primer domingo de noviembre (UTC-6)
+        private static TimeSpan GetMatamorosOffset(DateTime date)
+        {
+            int year = date.Year;
+
+            DateTime startDST = GetSecondSundayOfMarch(year);
+            DateTime endDST = GetFirstSundayOfNovember(year);
+
+            if (date >= startDST && date < endDST)
+                return TimeSpan.FromHours(-5); // Horario de verano
+            else
+                return TimeSpan.FromHours(-6); // Horario estándar
+        }
+
+        private static DateTime GetSecondSundayOfMarch(int year)
+        {
+            DateTime marchFirst = new DateTime(year, 3, 1);
+            int daysUntilSunday = ((int)DayOfWeek.Sunday - (int)marchFirst.DayOfWeek + 7) % 7;
+            DateTime firstSunday = marchFirst.AddDays(daysUntilSunday);
+            return firstSunday.AddDays(7); // segundo domingo
+        }
+
+        private static DateTime GetFirstSundayOfNovember(int year)
+        {
+            DateTime novemberFirst = new DateTime(year, 11, 1);
+            int daysUntilSunday = ((int)DayOfWeek.Sunday - (int)novemberFirst.DayOfWeek + 7) % 7;
+            return novemberFirst.AddDays(daysUntilSunday); // primer domingo
         }
     }
 }
